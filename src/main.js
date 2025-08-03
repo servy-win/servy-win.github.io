8110f54a07a48f49a19d036712f748b187f784f1
aelassas <aelassas@users.noreply.github.com>
2025-08-03 07:52:21 +0000

    deploy: c4278b1fcd1cffe0416e63f51cf28513d30afc5b

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-window.addEventListener('DOMContentLoaded', () => {
-  // Set the current year in the footer
-  const yearElement = document.getElementById('year')
-  if (yearElement) {
-    yearElement.textContent = new Date().getFullYear()
-  }
-})
