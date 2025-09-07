f6cfb954668fc2cab2fb4a01ccc824896a77f25b
aelassas <akram.elassas@gmail.com>
2025-09-07 13:20:11 +0100

    fix: contact form

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,26 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   })
 
+  // contact form
+  const form = document.getElementById('contact-form')
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+    const response = await fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: { 'Accept': 'application/json' }
+    })
+
+    if (response.ok) {
+      form.reset() // clear all fields silently
+    } else {
+      alert('Oops! There was a problem submitting your form.')
+    }
+  })
+
   // Set the current year in the footer
   const yearElement = document.getElementById('year')
   if (yearElement) {
